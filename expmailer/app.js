9c80ae275326860d544d76575b56b938fe99cf88
theWeboy <devdeepakbansal@gmail.com>
2018-04-02 15:44:59 +0530

    Add views

--- expmailer/app.js
+++ expmailer/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var routes = require('./routes/index');
+var about = require('./routes/about');
+var contact = require('./routes/contact');
 
 var app = express();
 
@@ -19,6 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/about',about);
+app.use('/contact',contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
